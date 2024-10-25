92ba8652d5340bbe96b655eb15751c80e10c7b6e
ericAleixo <ericaleixo66@gmail.com>
2024-10-25 18:49:00 -0300

    CHORE: primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,124 @@
+import express, { Request, Response } from "express"
+import { PrismaClient } from "@prisma/client"
+import cors from "cors"
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/client", async (req: Request, res: Response) => {
+
+    try {
+        const clientes = await prisma.clients.findMany()
+        res.status(200).json({
+            "mensagem": "Consulta realizada com sucesso!",
+            "query": clientes
+        })
+    } catch (error) {
+        res.status(400).json({
+            "mensagem": "Erro ao realizar a consulta.",
+            "query": error
+        })
+    } finally {
+        await prisma.$disconnect()
+    }
+
+})
+
+app.post("/client", async (req: Request, res: Response) => {
+
+    try {
+        const { nome, email, senha, vip, totalCompras } = req.body
+        await prisma.clients.create({
+            data: {
+                nome: nome,
+                email: email,
+                senha: senha,
+                vip: vip,
+                total_compras: totalCompras
+            }
+        })
+        res.status(201).json({
+            "mensagem": `Usuário "${nome}" cadastrado com sucesso!`
+        })
+    } catch (error) {
+        res.status(400).json({
+            "mensagem": "Erro ao realizar a consulta.",
+            "query": error
+        })
+    } finally {
+        prisma.$disconnect()
+    }
+
+})
+app.put("/client/:id", async (req: Request, res: Response) => {
+
+    try {
+        const idQuery = req.params.id
+        const idUser = Number(idQuery)
+        
+        const {nome, email, senha, vip, totalCompras } = req.body
+        await prisma.clients.update({
+            where: {
+                id: idUser
+            },
+            data: {
+                nome: nome,
+                email: email,
+                senha: senha,
+                vip: vip,
+                total_compras: totalCompras
+            }
+        })
+        res.status(201).json({
+            "mensagem": `Usuário "${nome}" atualizado com sucesso!`
+        })
+    } catch (error) {
+        res.status(400).json({
+            "mensagem": "Erro ao realizar a atualização.",
+            "query": error
+        })
+    } finally {
+        prisma.$disconnect()
+    }
+
+})
+
+app.delete("/client/:id", async (req: Request, res: Response)=>{
+
+    try{
+
+        const idQuery = req.params.id
+        const idUser = Number(idQuery)
+
+        const nome = await prisma.clients.findUnique({
+            where:{
+                id: idUser
+            }
+        })
+
+        await prisma.clients.delete({
+            where:{
+                id: idUser
+            }
+        })
+
+        res.status(201).json({
+            "mensagem": `Usuário ${nome?.nome} deletado com sucesso`
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            "mensagem": "Erro ao realizar a atualização.",
+            "query": error
+        })
+    } finally {
+        prisma.$disconnect()
+    }
+
+})
+
+const porta: Number = 3000
+app.listen(porta, () => console.log(`API no ar em 'https://localhost:${porta}'`))
