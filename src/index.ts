22091713ed4a37ff3a6affa10aefbdbaff4587a9
ericAleixo <ericaleixo66@gmail.com>
2024-10-29 12:22:27 -0300

    Teste: testando o origin

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'], 
   };
